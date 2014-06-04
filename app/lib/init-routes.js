34c34cf62ac1a630edebc809115459ab4601bf3f
Terrence <terrenceljones805@gmail.com>
2014-06-04 10:58:21 -0500

    added routes

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -15,8 +15,10 @@ module.exports = (req, res, next)=>{
 
 function load(app, fn){
   var home = traceur.require(__dirname + '/../routes/home.js');
+  var shows = traceur.require(__dirname + '/../routes/shows.js');
 
   app.get('/', dbg, home.index);
+  app.get('/show/past/:id', dbg, shows.past);
   console.log('Routes Loaded');
   fn();
 }
---
+++ app/routes/shows.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.index = (req, res)=>{
+  res.render('shows/past', {title: 'Node.js: Home'});
+};
