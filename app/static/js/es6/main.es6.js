147f29579b9b89bd3163f0d46a49a2713ffbd96f
asejdina <asejdina@gmail.com>
2014-06-04 14:58:39 -0500

    main.es6 and styling

--- app/static/js/es6/main.es6.js
+++ app/static/js/es6/main.es6.js
@@ -11,49 +11,73 @@
   var charts = {};
 
   function init(){
-    // initDate();
-    initMap(36, -85, 7);
+    initMap(36, -85, 6);
     demoMarker();
-    // $('#add').click(show);
-    // $('#add').click(get);
-  }
+    getFutureShows();
+    getPastShows();
+    $('#pastShows').on('click', '.pastShow', showImages);
 
-  // function initDate(){
-  //   var fmt = moment().format('LL');
-  //   $('#date').append(fmt);
-  // }
+  }
 
 //=============================================Map functions
   var map;
 
   function initMap(lat, lng, zoom){
-    // let styles = [{'stylers':[{'visibility':'off'}]},{'featureType':'water','stylers':[{'visibility':'on'},{'color':'#2f343b--'}]},{'featureType':'landscape','stylers':[{'visibility':'on'},{'color':'#514ED9'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'visibility':'on'},{'color':'#2f343b'},{'weight':1}]}];
-    let mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: null};//SATELITE or HYBRID are options instead of ROADMAP.
+    let mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: null};
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
   function addMarker(lat, lng, name, icon){
     let latLng = new google.maps.LatLng(lat, lng);
-    new google.maps.Marker({map: map, position: latLng, title: name, icon: icon});
-  }
-
-  function show(){
-    let location = $('#location').val().trim();
-    let geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode({address: location}, (results, status)=>{
-      let name = results[0].formatted_address;
-      let lat = results[0].geometry.location.lat();
-      let lng = results[0].geometry.location.lng();
-      addMarker(lat, lng, name, './media/map-marker.svg');
-      let latLng = new google.maps.LatLng(lat, lng);
-      map.setCenter(latLng); //------------centers the map on the object you want
-      map.setZoom(6);
-    });
+    new google.maps.Marker({map: map, position: latLng, title: name, icon: '/img/map-marker.svg'});
   }
 
   function demoMarker(){
     addMarker(36.137076, -86.778557, 'The Basement');
+  }
+
+//============================================Shows Past and Future
+
+  function getFutureShows(){
+      $.ajax({
+        url: '/',
+        type: 'GET',
+        data: null,
+        dataType: 'json',
+        success: futureShows=>{
+          console.log(futureShows);
+          futureShows.futureShows.forEach(s=>{
+            var lat = s.latitude;
+            var lon = s.longitude;
+            var name = s.venueName;
+            addMarker(lat, lon, name.toString());
+          });
+        }
+      });
+    }
+
+  function getPastShows(){
+      $.ajax({
+        url: '/',
+        type: 'GET',
+        data: null,
+        dataType: 'json',
+        success: pastShows=>{
+          console.log(pastShows);
+          pastShows.pastShows.forEach(s=>{
+            var lat = s.latitude;
+            var lon = s.longitude;
+            var name = s.venueName;
+            addMarker(lat, lon, name.toString());
+          });
+        }
+      });
+    }
+
+//===============================================Past Show Images
+
+  function showImages(){
+    
   }
 
 
